957b492c3b2ebbc26f57688af4e5f7ee4f0faf74
jn-prod <nicolas.jouanno@gmail.com>
2018-05-30 09:36:39 +0200

    mentions legales

--- app/router/cmsRoutes.js
+++ app/router/cmsRoutes.js
@@ -7,5 +7,7 @@ var cmsCtrl = require('../controllers/cmsController')
 // ---------------- INDEX ----------------
 // Get Homepage
 router.get('/', cmsCtrl.index);
+// Get Mentions Légales
+router.get('/mentions-legales', cmsCtrl.mentionsLegales);
 
 module.exports = router;
