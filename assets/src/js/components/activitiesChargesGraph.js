7f5011b68eee2269212ebc9c673fda0cde0a93b5
jn-prod <nicolas.jouanno@gmail.com>
2018-06-05 14:47:55 +0200

    activities graph

---
+++ assets/src/js/components/activitiesChargesGraph.js
@@ -0,0 +1,56 @@
+import Chart from 'chart.js'
+
+if (document.getElementById('healthScoreGraph')) {
+  var ctx = document.getElementById('healthScoreGraph').getContext('2d')
+  var labels = []
+  var score = []
+
+  $('input.activitiesWeek').each((key, val) => {
+    labels.push(val.value)
+    $(val).remove()
+  })
+
+  $('input.activitiesCharge').each((key, val) => {
+    score.push(val.value)
+    $(val).remove()
+  })
+
+  var myChart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'bar',
+    // The data for our dataset
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Score',
+        data: score,
+        backgroundColor: [
+          'rgba(0, 180, 177, 0.5)',
+          'rgba(0, 180, 177, 0.5)',
+          'rgba(0, 180, 177, 0.5)',
+          'rgba(0, 180, 177, 0.5)',
+          'rgba(0, 180, 177, 0.5)'
+        ],
+        borderColor: [
+          'rgba(0,144,142,1)',
+          'rgba(0,144,142,1)',
+          'rgba(0,144,142,1)',
+          'rgba(0,144,142,1)',
+          'rgba(0,144,142,1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  })
+}
+
+export default myChart
--- assets/src/js/components/healthScoreGraph.js
+++ assets/src/js/components/healthScoreGraph.js
@@ -5,57 +5,59 @@ var url = window.document.URL
 var id = url.split('/')[url.split('/').length - 1]
 var json
 
-var ctx = document.getElementById('scoreHealthChart').getContext('2d')
-var myChart
+if (document.getElementById('scoreHealthChart')) {
+  var ctx = document.getElementById('scoreHealthChart').getContext('2d')
+  var myChart
 
-if (domain === 'localhost') {
-  json = 'https://localhost:3000/health/data/' + id
-} else {
-  json = 'https://www.feezeify.me/health/data/' + id
-}
+  if (domain === 'localhost') {
+    json = 'https://localhost:3000/health/data/' + id
+  } else {
+    json = 'https://www.feezeify.me/health/data/' + id
+  }
 
-$.getJSON(json, (res) => {
-  var healthScore = res
-  var score = Object.values(healthScore.health_detail)
-  var labels = Object.keys(healthScore.health_detail)
+  $.getJSON(json, (res) => {
+    var healthScore = res
+    var score = Object.values(healthScore.health_detail)
+    var labels = Object.keys(healthScore.health_detail)
 
-  myChart = new Chart(ctx, {
-    // The type of chart we want to create
-    type: 'horizontalBar',
+    myChart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'horizontalBar',
 
-    // The data for our dataset
-    data: {
-      labels: labels,
-      datasets: [{
-        label: 'Score',
-        data: score,
-        backgroundColor: [
-          'rgba(0, 180, 177, 0.5)',
-          'rgba(0, 180, 177, 0.5)',
-          'rgba(0, 180, 177, 0.5)',
-          'rgba(0, 180, 177, 0.5)',
-          'rgba(0, 180, 177, 0.5)'
-        ],
-        borderColor: [
-          'rgba(0,144,142,1)',
-          'rgba(0,144,142,1)',
-          'rgba(0,144,142,1)',
-          'rgba(0,144,142,1)',
-          'rgba(0,144,142,1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        xAxes: [{
-          ticks: {
-            beginAtZero: true
-          }
+      // The data for our dataset
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Score',
+          data: score,
+          backgroundColor: [
+            'rgba(0, 180, 177, 0.5)',
+            'rgba(0, 180, 177, 0.5)',
+            'rgba(0, 180, 177, 0.5)',
+            'rgba(0, 180, 177, 0.5)',
+            'rgba(0, 180, 177, 0.5)'
+          ],
+          borderColor: [
+            'rgba(0,144,142,1)',
+            'rgba(0,144,142,1)',
+            'rgba(0,144,142,1)',
+            'rgba(0,144,142,1)',
+            'rgba(0,144,142,1)'
+          ],
+          borderWidth: 1
         }]
+      },
+      options: {
+        scales: {
+          xAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
       }
-    }
+    })
   })
-})
+}
 
 export default myChart
