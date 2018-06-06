c57f409f1b935ba8d063120c3a6cf609abc7ca3d
jn-prod <nicolas.jouanno@gmail.com>
2018-06-06 13:05:47 +0200

    test corr

--- app/models/user.js
+++ app/models/user.js
@@ -5,13 +5,15 @@ var mongoose = require('mongoose');
 var UserSchema = mongoose.Schema({
   strava_id: { type: Number },
   facebook_id: { type: Number },
-  username: { type: String, index:true },
-  email: { type: String, unique: true, required: true },
+  username: { type: String},
+  email: { type: String, unique: true, required: true, index:true },
   firstname: { type: String },
   lastname : { type: String },
   sex: { type: String },
   country: { type: String },
   city: { type: String },
+  size: { type: Number },
+  date_of_birth: { type: Date },
 
   //log
   created_at : { type: Date, required: true, default: Date.now },
