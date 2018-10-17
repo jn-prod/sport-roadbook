a1742e0e320886608c1b9c99ac5c18f1cd97dbf3
jn-prod <nicolas.jouanno@gmail.com>
2018-10-17 16:15:58 +0200

    ip location in progress

--- assets/src/js/components/getLocation.js
+++ assets/src/js/components/getLocation.js
@@ -1,6 +1,6 @@
 var getLocation = (locationCb) => {
-  $.getJSON('http://api.ipapi.com/check?access_key=47205444bce06ace12eb9468f268109e', (data) => {
-    console.log(data)
+  $.getJSON('http://api.ipapi.com/' + $('input[name=ip]').val() + '?access_key=47205444bce06ace12eb9468f268109e', (data) => {
+    // console.log(data)
     var location = {latitude: data.location.latitude, longitude: data.location.longitude}
     locationCb(location)
   })
