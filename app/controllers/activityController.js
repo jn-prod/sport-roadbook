c19f2c8288d86f3d80dbea8989c4d8bd907200f5
Nicolas Jouanno <nicolas.jouanno@gmail.com>
2018-11-01 19:16:37 +0100

    Update app/controllers/activityController.js

--- app/controllers/activityController.js
+++ app/controllers/activityController.js
@@ -24,19 +24,19 @@ var activityCtrl = {
     form.moving_time = convertTime(form.moving_time_hours, form.moving_time_minutes, form.moving_time_seconds)
 
     // calcul du TSS
-    if (form.moving_time * 1 > 0 && form.fc_moyenne * 1 > 0) {
-      // definition du seuil
-      var seuil
-      if (form.user.fc_max !== undefined) {
-        seuil = form.user.fc_max * 0.80
-      } else if (form.user.date_of_birth) {
-        seuil = (220 - (today.getFullYear() - form.user.date_of_birth.getFullYear())) * 0.80
-      } else {
-        seuil = 200 * 0.80
-      }
-      var intensityFactor = form.fc_moyenne / seuil
-      form.tss = ((form.moving_time * form.fc_moyenne * intensityFactor) / (seuil * 3600)) * 100
-    }
+    // if (form.moving_time * 1 > 0 && form.fc_moyenne * 1 > 0) {
+    //   // definition du seuil
+    //   var seuil
+    //   if (form.user.fc_max !== undefined) {
+    //     seuil = form.user.fc_max * 0.80
+    //   } else if (form.user.date_of_birth) {
+    //     seuil = (220 - (today.getFullYear() - form.user.date_of_birth.getFullYear())) * 0.80
+    //   } else {
+    //     seuil = 200 * 0.80
+    //   }
+    //   var intensityFactor = form.fc_moyenne / seuil
+    //   form.tss = ((form.moving_time * form.fc_moyenne * intensityFactor) / (seuil * 3600)) * 100
+    // }
 
     var newActivity = new Activity(form)
     newActivity.save((err, activity) => {
