69657fa8c9b63838d0e9f5a199158cdd22d16871
Nicolas Jouanno <nicolas.jouanno@gmail.com>
2019-06-14 21:52:49 +0200

    Update domain-check.js

--- custom_modules/domain-check.js
+++ custom_modules/domain-check.js
@@ -3,7 +3,7 @@ var domainUrl
 if (process.env.LOCAL) {
   domainUrl = 'https://localhost:3000'
 } else {
-  domainUrl = 'https://app.nicolasjouanno.com'
+  domainUrl = 'https://feezify-dev.herokuapp.com'
 }
 
 module.exports = domainUrl
